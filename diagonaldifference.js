6bddbab1d627f012ca580b2fc0f974d99fac3532
Icaro Nascimento <icaronascimento1@gmail.com>
2019-05-06 10:39:49 -0300

    diagonaldifference.js: add diagonaldifference iterative solution

--- diagonaldifference.js
+++ diagonaldifference.js
@@ -1,30 +1,44 @@
 const assert = require('assert');
 
 function diagonalDifference(inputMatrix) {
-	// let totalSum = inputMatrix.reduce(function(sum, value) {
-	// 	return sum + value;
-	// });
+	// let leftToRight = inputMatrix[0][0] + inputMatrix[1][1] + inputMatrix[2][2];
+	// let rightToLeft = inputMatrix[0][2] + inputMatrix[1][1] + inputMatrix[2][0];
 
-    const leftToRight = inputMatrix[0][0] + inputMatrix[1][1] + inputMatrix[2][2] 
-    const rightToLeft = inputMatrix[0][2] + inputMatrix[1][1] + inputMatrix[2][0] 
-    return Math.abs(leftToRight-rightToLeft);
+		let leftToRight = 0
+		let rightToLeft = 0
+	
+	for (let row = 0; row < inputMatrix.length; row++) {
+		 leftToRight += inputMatrix[row][row];
+		 rightToLeft += inputMatrix[row][inputMatrix.length - row - 1];
+	}
+
+	return Math.abs(leftToRight - rightToLeft);
 }
 
-assert.equal(diagonalDifference([
-    [ 1, 2, 3 ],
-    [ 4, 5, 6 ],
-    [ 7, 8, 9 ],
-]), 0);
-assert.equal(diagonalDifference([
-    [ 1, 2, 3 ],
-    [ 4, 5, 6 ],
-    [ 9, 8, 9 ],
-]), 2);
-assert.equal(diagonalDifference([
-    [ 11, 2, 4 ],
-    [ 4, 5, 6 ],
-    [ 10, 8, -12 ],
-]), 15);
+assert.equal(diagonalDifference([ [ 1, 2, 3 ], 
+								  [ 4, 5, 6 ], 
+								  [ 7, 8, 9 ] ]), 0);
+
+assert.equal(diagonalDifference([ [ 1, 2, 3 ], 
+								  [ 4, 5, 6 ], 
+								  [ 9, 8, 9 ] ]), 2);
+
+assert.equal(diagonalDifference([ [ 11, 2, 4 ], 
+								  [ 4, 5, 6 ], 
+								  [ 10, 8, -12 ] ]), 15);
+
+assert.equal(diagonalDifference([ [ -11, 2, 4 ], 
+								  [ 4, -5, 6 ], 
+								  [ 10, -8, -12 ] ]), 37);
+
+assert.equal(diagonalDifference([ [ 1, 2, 3, 4 ], 
+								  [ 5, 6, 7, 8 ], 
+								  [ 9, 10, 11, 12 ], 
+								  [ 0, 14, 15, 16 ] ]), 13);
+
+assert.equal(diagonalDifference([ [ 1, 2, 3, 4 ], 
+								  [ 5, 6, 7, 8 ], 
+								  [ 9, 10, 11, 12 ], 
+								  [ 13, 14, 15, 17 ] ]), 1);
 
 console.log('ok');
-
