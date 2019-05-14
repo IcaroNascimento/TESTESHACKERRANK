79fa7c251c6b69fb30c8a29eca230e7059e94469
Icaro Nascimento <icaronascimento1@gmail.com>
2019-05-14 16:30:54 -0300

    utopiantree.js: refactor to ternary operator validation

--- utopiantree.js
+++ utopiantree.js
@@ -1,11 +1,7 @@
 const assert = require('assert');
 
-function nextHeight(i, prevheight) {
-	if (prevheight % 2 === 0) {
-		prevheight += 1;
-	} else {
-		prevheight *= 2;
-	}
+function height(i, prevheight) {
+	prevheight % 2 === 0 ? (prevheight += 1) : (prevheight *= 2);
 	return prevheight;
 }
 
@@ -14,7 +10,7 @@ function utopianTree(n) {
 	let result = [];
 
 	for (let i = 0; i <= n; i++) {
-		const actualHeight = nextHeight(i, prevHeight);
+		const actualHeight = height(i, prevHeight);
 		result.push(actualHeight);
 		prevHeight = actualHeight;
 	}
@@ -22,12 +18,12 @@ function utopianTree(n) {
 	return result[result.length - 1];
 }
 
-assert.equal(nextHeight(0, 0), 1);
-assert.equal(nextHeight(1, 1), 2);
-assert.equal(nextHeight(2, 2), 3);
-assert.equal(nextHeight(3, 3), 6);
-assert.equal(nextHeight(4, 6), 7);
-assert.equal(nextHeight(5, 7), 14);
+assert.equal(height(0, 0), 1);
+assert.equal(height(1, 1), 2);
+assert.equal(height(2, 2), 3);
+assert.equal(height(3, 3), 6);
+assert.equal(height(4, 6), 7);
+assert.equal(height(5, 7), 14);
 
 assert.equal(utopianTree(0), 1);
 assert.equal(utopianTree(1), 2);
