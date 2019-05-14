c083c995d7aa5fa44d60da875d6d94116359d21e
Icaro Nascimento <icaronascimento1@gmail.com>
2019-05-14 17:56:30 -0300

    utopianTree.js: refactoring master

--- utopiantree.js
+++ utopiantree.js
@@ -1,29 +1,13 @@
 const assert = require('assert');
 
-function height(i, prevheight) {
-	prevheight % 2 === 0 ? (prevheight += 1) : (prevheight *= 2);
-	return prevheight;
-}
-
 function utopianTree(n) {
-	let prevHeight = 0;
-	let result = [];
+	let height = 1;
 
-	for (let i = 0; i <= n; i++) {
-		const actualHeight = height(i, prevHeight);
-		result.push(actualHeight);
-		prevHeight = actualHeight;
-	}
+	for (let i = 1; i <= n; i++)
+		height += (height % 2 ? height : 1);
 
-	return result[result.length - 1];
+	return height;
 }
-
-assert.equal(height(0, 0), 1);
-assert.equal(height(1, 1), 2);
-assert.equal(height(2, 2), 3);
-assert.equal(height(3, 3), 6);
-assert.equal(height(4, 6), 7);
-assert.equal(height(5, 7), 14);
 
 assert.equal(utopianTree(0), 1);
 assert.equal(utopianTree(1), 2);
