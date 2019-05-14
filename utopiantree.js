1edbd3b959e72c9652c1040051d5adcc6e615dfe
Icaro Nascimento <icaronascimento1@gmail.com>
2019-05-14 15:42:12 -0300

    utopiantree.js: add function nextHeight to interate previous height with actual height

--- utopiantree.js
+++ utopiantree.js
@@ -1,13 +1,33 @@
 const assert = require('assert');
 
-function utopianTree(n) {
-	let height = 0;
-	for (let i = 0; i < n; i++) {
-		height = height + (i / 2 !== 0 ? 1 : height);
+function nextHeight(i, prevheight) {
+	if (prevheight % 2 === 0) {
+		prevheight += 1;
+	} else {
+		prevheight *= 2;
 	}
-	return height;
+	return prevheight;
 }
 
+function utopianTree(n) {
+	let prevHeight = 0;
+	let result = [];
+
+	for (let i = 0; i <= n; i++) {
+		const actualHeight = nextHeight(i, prevHeight);
+		result.push(actualHeight);
+		prevHeight = actualHeight;
+	}
+
+	return result[result.length - 1];
+}
+
+assert.equal(nextHeight(0, 0), 1);
+assert.equal(nextHeight(1, 1), 2);
+assert.equal(nextHeight(2, 2), 3);
+assert.equal(nextHeight(3, 3), 6);
+assert.equal(nextHeight(4, 6), 7);
+assert.equal(nextHeight(5, 7), 14);
 
 assert.equal(utopianTree(0), 1);
 assert.equal(utopianTree(1), 2);
