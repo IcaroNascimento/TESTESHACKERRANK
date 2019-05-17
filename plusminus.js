5056e8a5b0dc7250a208daa31b214b32c8171a98
Icaro Nascimento <icaronascimento1@gmail.com>
2019-05-17 17:16:02 -0300

    plusminus.js: add test to countPositive, countNegative and countZero

--- plusminus.js
+++ plusminus.js
@@ -1,14 +1,47 @@
 const assert = require('assert');
 
 function plusMinus(array) {
-	return (0.400000, 
-            0.400000,
-            0.200000);
+
+const arrayString = array.toString()
+countNegative = arrayString.split(/[-]/g).length - 1
+countZero = arrayString.split(/ˆ0/g).length 
+countPositive = array.length - (countNegative + countZero)
+
+
+// decimalcountNegative = countNegative.toFloat()/array.length
+// decimalcountZero
+
+return `${countPositive}, ${countNegative}, ${countZero}`
+    // if (array.length === 5){
+
+	// return ([0.400000, 
+    //          0.400000,
+    //          0.200000]);
+
+    // } else{
+    //     return ([0.500000, 
+    //              0.333333,
+    //              0.166667]);
+    // }
 
 }
 
-assert.equal(plusMinus([ 1, 1, 0, -1, -1 ]), (0.400000, 
-                                              0.400000,
-                                              0.200000));
+assert.equal(plusMinus([ 1, 1, 0, -1, -1 ]),'2, 2, 1')
+
+// assert.deepEqual(plusMinus([ 1, 1, 0, -1, -1 ]),[0.400000, 
+//                                                  0.400000,
+//                                                  0.200000]);
+
+
+// assert.deepEqual(plusMinus([ -4, 3, -9, 0, 4, 1  ]),[0.500000, 
+//                                                      0.333333,
+//                                                      0.166667]);
+
+// assert.deepEqual(plusMinus([ -4, 3, -9, 0, 4, 1 , 1 ]),[0.571428, 
+//                                                         0.285714,
+//                                                         0.166667]);
+
+
+
 
 console.log('Ok')
