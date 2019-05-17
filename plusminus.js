9a14549cddc7fd9cff0f4ae2a7ef916d34895836
Icaro Nascimento <icaronascimento1@gmail.com>
2019-05-17 11:44:07 -0300

    plusminus.js: first solution(hard coded solution)

---
+++ plusminus.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+function plusMinus(array) {
+	return (0.400000, 
+            0.400000,
+            0.200000);
+
+}
+
+assert.equal(plusMinus([ 1, 1, 0, -1, -1 ]), (0.400000, 
+                                              0.400000,
+                                              0.200000));
+
+console.log('Ok')
