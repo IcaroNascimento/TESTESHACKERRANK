73497b2a64909915bbca1303a36f768073b1109d
Icaro Nascimento <icaronascimento1@gmail.com>
2019-05-19 22:33:36 -0300

    plusminus.js: include solution

--- plusminus.js
+++ plusminus.js
@@ -1,47 +1,17 @@
 const assert = require('assert');
 
 function plusMinus(array) {
+	const arrayString = array.toString();
+	const countNegative = arrayString.split(/[-]/g).length - 1;
+	const countZero = arrayString.split(/[0]/g).length - 1;
+	const countPositive = array.length - (countNegative + countZero);
 
-const arrayString = array.toString()
-countNegative = arrayString.split(/[-]/g).length - 1
-countZero = arrayString.split(/ˆ0/g).length 
-countPositive = array.length - (countNegative + countZero)
-
-
-// decimalcountNegative = countNegative.toFloat()/array.length
-// decimalcountZero
-
-return `${countPositive}, ${countNegative}, ${countZero}`
-    // if (array.length === 5){
-
-	// return ([0.400000, 
-    //          0.400000,
-    //          0.200000]);
-
-    // } else{
-    //     return ([0.500000, 
-    //              0.333333,
-    //              0.166667]);
-    // }
-
+	return `${(countPositive / array.length).toFixed(6)}, ${(countNegative / array.length).toFixed(6)}, ${(countZero /
+		array.length).toFixed(6)}`;
 }
 
-assert.equal(plusMinus([ 1, 1, 0, -1, -1 ]),'2, 2, 1')
+assert.equal(plusMinus([ 1, 1, 0, -1, -1 ]), '0.400000, 0.400000, 0.200000');
+assert.equal(plusMinus([ 1, 2, 3, -1, -2, -3, 0, 0 ]), '0.375000, 0.375000, 0.250000');
+assert.equal(plusMinus([ -4, 3, -9, 0, 4, 1 ]), '0.500000, 0.333333, 0.166667');
 
-// assert.deepEqual(plusMinus([ 1, 1, 0, -1, -1 ]),[0.400000, 
-//                                                  0.400000,
-//                                                  0.200000]);
-
-
-// assert.deepEqual(plusMinus([ -4, 3, -9, 0, 4, 1  ]),[0.500000, 
-//                                                      0.333333,
-//                                                      0.166667]);
-
-// assert.deepEqual(plusMinus([ -4, 3, -9, 0, 4, 1 , 1 ]),[0.571428, 
-//                                                         0.285714,
-//                                                         0.166667]);
-
-
-
-
-console.log('Ok')
+console.log('Ok');
