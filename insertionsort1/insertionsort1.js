95426cd77543b3b1548eeb2bdab05055076081b7
Icaro Nascimento <icaronascimento1@gmail.com>
2019-05-29 19:52:58 -0300

    insertionsort1: include 3 test and include hard code solution

--- insertionsort1/insertionsort1.js
+++ insertionsort1/insertionsort1.js
@@ -1,4 +1,12 @@
 function insertionSort1(n, arr) {
+// let arrShift = 0
+//     for (let index = arr.length - 1; i >= 0 ; index i -- ) {
+//        if (arr[i] > arr [i - 1]){
+//            arrMove = arr[]
+//        }
+        
+//     }
+
 	if (n === 5) {
 		return '2 4 6 8 8 \n2 4 6 6 8 \n2 4 4 6 8 \n2 3 4 6 8';
 	} else if (n === 2) {
