536eb36b376a95286f74253913e518871429fa0b
Icaro Nascimento <icaronascimento1@gmail.com>
2019-06-07 10:27:34 -0300

    insertionsort1: include tests with jest

--- insertionsort1/insertionsort1.test.js
+++ insertionsort1/insertionsort1.test.js
@@ -3,9 +3,15 @@ const insertionSort1 = require('./insertionsort1');
 test('when the sequency  is "2, 4, 6, 8, 3", return the output  "\n2 4 6 8 8 \n2 4 6 6 8 \n2 4 4 6 8 \n2 3 4 6 8"', () => {
 	expect(insertionSort1( 5,[2, 4, 6, 8, 3])).toEqual('2 4 6 8 8 \n2 4 6 6 8 \n2 4 4 6 8 \n2 3 4 6 8');
 });
-test('when the sequency  is "2, 4, 6, 8, 7", return the output  "\n2 4 6 8 8 \n2 4 6 7 8"', () => {
-	expect(insertionSort1(5, [2, 4, 6, 8, 7])).toEqual('2 4 6 8 8 \n2 4 6 7 8');
+test('when the sequency  is "2, 4, 6, 8, 1", return the output  "\n2 4 6 8 8 \n2 4 6 6 8 \n2 4 4 6 8 \n2 2 4 6 8 \n1 2 4 6 8"', () => {
+	expect(insertionSort1(5, [2, 4, 6, 8, 1])).toEqual('2 4 6 8 8 \n2 4 6 6 8 \n2 4 4 6 8 \n2 2 4 6 8 \n1 2 4 6 8');
 });
-test('when the sequency  is "2, 3, 4, 7, 8", return the output  "2 3 4 7 8"', () => {
-	expect(insertionSort1(5 [2, 3, 4, 7, 8 ])).toEqual('2 3 4 7 8');
+test('when the sequency  is "2, 4, 6, 8, 6", return the output  "\n2 4 6 8 8 \n2 4 6 6 8"', () => {
+	expect(insertionSort1(5, [2, 4, 6, 8, 6 ])).toEqual('2 4 6 8 8 \n2 4 6 6 8');
+});
+test('when the sequency  is "2, 4, 6, 8, 10", return the output  "\n2 4 6 8 10"', () => {
+	expect(insertionSort1(5, [2, 4, 6, 8, 10 ])).toEqual('2 4 6 8 10');
+});
+test('when the sequency  is "1", return the output  "\n1"', () => {
+	expect(insertionSort1(1, [1])).toStrictEqual('1');
 });
