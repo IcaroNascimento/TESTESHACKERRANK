82063116c968eb81e21a12c4267585299bee4ee1
Icaro Nascimento <icaronascimento1@gmail.com>
2019-06-13 13:55:11 -0300

    circulararrayrotation: include performance optimization to select query index

--- circulararrayrotation/circulararrayrotation.js
+++ circulararrayrotation/circulararrayrotation.js
@@ -1,20 +1,42 @@
 const assert = require('assert');
 
-function circularArrayRotation(a, k, queries) {
-	const meta = k % a.length;
-
-	for (let index = 0; index < meta; index++) {
-		a.unshift(a.pop());
-	}
-	const result = [];
-	queries.forEach((index) => {
-		result.push(a[index]);
+function selecionarResultado(elementos, indices) {
+	const resultados = [];
+	indices.forEach((index) => {
+		resultados.push(elementos[index]);
 	});
-	return result.join(' \n');
+	return resultados.join(' \n');
+}
+
+function transformaIndice(tamanho, rotacoes) {
+	const rotacoesLimitadas = rotacoes % tamanho;
+	return function(indiceSolicitado) {
+		let indiceFinal = indiceSolicitado - rotacoesLimitadas;
+		if (indiceFinal < 0)
+			indiceFinal = tamanho + indiceFinal;
+		return indiceFinal;
+	}
+}
+
+function circularArrayRotation(elementos, rotacoes, queries) {
+	const transformador = transformaIndice(elementos.length, rotacoes);
+	const indices = queries.map(transformador);
+	return selecionarResultado(elementos, indices);
 }
 
 module.exports = circularArrayRotation;
 
-assert.deepEqual(circularArrayRotation([ 1, 2, 3 ], 2, [ 0, 1, 2 ]), '2 \n3 \n1');
+const ABC = 'abc'.split('');
+assert.deepEqual(circularArrayRotation(ABC, 0, [ 0 ]), 'a');
+assert.deepEqual(circularArrayRotation(ABC, 0, [ 1 ]), 'b');
+assert.deepEqual(circularArrayRotation(ABC, 1, [ 1 ]), 'a');
+assert.deepEqual(circularArrayRotation(ABC, 1, [ 0 ]), 'c');
+assert.deepEqual(circularArrayRotation(ABC, 3, [ 0 ]), 'a');
+
+const ABCDEF = 'abcdef'.split('');
+assert.deepEqual(circularArrayRotation(ABCDEF, 1, [ 5 ]), 'e');
+assert.deepEqual(circularArrayRotation(ABCDEF, 5, [ 5 ]), 'a');
+assert.deepEqual(circularArrayRotation(ABCDEF, 6, [ 5 ]), 'f');
+assert.deepEqual(circularArrayRotation(ABCDEF, 12, [ 5 ]), 'f');
 
 console.log('ok');
