03a1530692dfb362fb77c3da43420b0042c2edac
Icaro Nascimento <icaronascimento1@gmail.com>
2019-06-25 18:15:32 -0300

    missingnumbers: WIP include first solution

--- missingnumbers/missingnumbers.js
+++ missingnumbers/missingnumbers.js
@@ -1,17 +1,28 @@
 function getElementOccurrences(array) {
-	let obj = {}
-	for (let i = 0; i < array.length; i++) { 
-		if (obj[array[i]] = obj[array[i]]) {
-			obj[array[i]] += 1 
-			}
-			else{
-			 obj[array[i]] = 1
-			}	
+	let obj = {};
+	for (let i = 0; i < array.length; i++) {
+		const key = array[i];
+		const value = obj[key];
+		obj[key] = value ? value + 1 : 1;
 	}
 
-	return obj
-
+	return obj;
 }
 
+function missingNumbers(arrayA, arrayB) {
+	let occurrencesArrayA = getElementOccurrences(arrayA);
+	let occurrencesArrayB = getElementOccurrences(arrayB);
+	let entriesA = Object.entries(occurrencesArrayA);
+	let entriesB = Object.entries(occurrencesArrayB);
+	let result = [];
+	for (let i = 0; i < entriesB.length; i++) {
+		if (entriesB[i][1] !== entriesA[i][1]) {
+			let arraKeyNumber = Number(entriesB[i][0]);
+			result.push(arraKeyNumber);
+		}
+	}
 
-module.exports = { getElementOccurrences };
+	return result;
+}
+
+module.exports = { getElementOccurrences, missingNumbers };
